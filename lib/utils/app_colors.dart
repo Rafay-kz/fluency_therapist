b2b121074b61c6c8cf460e120fffc9e51fb2baf6
Taha <m.7taharajpoot@gmail.com>
2023-05-10 16:51:51 +0500

    Set the project and deleted Windows, Linux and Macos folders.
    Added all app colors.

--- lib/utils/app_colors.dart
+++ lib/utils/app_colors.dart
@@ -2,7 +2,18 @@ import 'package:flutter/material.dart';
 
 //created by Abdul Rafay on 1-5-2023
 class AppColors {
-  static Color primary = const Color(0xff21132F);
-  static Color whiteColor = Colors.white;
-  static Color whitesblue = const Color(0xFFE3F2FD);
+  static Color primaryBlue =
+  const Color(0xff7FB2F0); // BLue- Main color buttons etc.
+  static Color secondaryBlue =
+  const Color(0xffD6E6F9); // Light blue color, containers etc.
+  static Color textColor = const Color(0xff000000); // Black - Main text color
+  static Color backgroundColor =
+  const Color(0xffF1F5FB); // Scaffold background color
+  static Color textHintColor =
+  const Color(0xff403B3B).withOpacity(0.5); //textfield text hint color
+  static Color textfieldColor =
+  const Color(0xffE6ECF6); //textfied background color
+  static Color descriptionColor = const Color(0xff000000)
+      .withOpacity(0.4); // description color under text color
+  static Color whiteColor = const Color(0xffFFFFFF); //white color for button "login " etc.
 }
