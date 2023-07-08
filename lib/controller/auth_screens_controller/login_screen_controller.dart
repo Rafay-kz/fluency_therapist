75945266164cb8c664dde7515b09fe359b706ffa
Taha <m.7taharajpoot@gmail.com>
2023-07-08 18:39:06 +0500

    - Connected firebase with the app - Connected Signup to firebase - Re did the validations - Added flutter toast to show errors.

--- lib/controller/auth_screens_controller/login_screen_controller.dart
+++ lib/controller/auth_screens_controller/login_screen_controller.dart
@@ -1,23 +1,49 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import '../../utils/app_constants.dart';
+
 //created by Bilal on 10-5-2023
 
 class LoginScreenController extends GetxController {
+
+  final formKey = GlobalKey<FormState>();
   TextEditingController emailTEController = TextEditingController();
   TextEditingController passwordTEController = TextEditingController();
-
   RxBool obscureText = true.obs;
 
-  void onLoginTap() {
+  var email = '';
+  var password = '';
 
-    GetUtils.isEmail(emailTEController.text)
-        ? print('Valid')
-        : print('Invalid Email');
-    GetUtils.isAlphabetOnly(passwordTEController.text)
-        ? print('Valid ')
-        : print('invalid Password');
-
-
+ @override
+  void onClose() {
+ emailTEController.dispose();
+ passwordTEController.dispose();
   }
+
+String ? validateEmail (String value) {
+   if(!GetUtils.isEmail(value)) {
+     return 'Provide valid email';
+   }
+   return null;
+}
+
+  String ? validatePassword (String value) {
+    if(value.length < 6) {
+      return 'Password must be of 6 characters';
+    }
+    return null;
+  }
+
+  void onLoginTap() {
+    if (formKey.currentState!.validate()) {
+      formKey.currentState!.save();
+      Get.toNamed(kHomeScreen);
+    } else {
+      // Form is invalid, display error messages or take appropriate actions
+    }
+  }
+
+
+
 }
